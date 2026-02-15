e51c8c7d191b14ceabc22acd28ec03264893a6f8
Holi66611 <michal.m.kazmierczak@gmail.com>
2026-02-15 17:14:26 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
-
 const nextConfig = {
-  turbopack: {},
-
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
-  },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
